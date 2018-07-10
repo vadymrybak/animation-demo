e8f6e67e8525465dcbd71c177682b66a5a112037
vrybak <vrybak@researchnow.com>
2018-07-10 11:09:31 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myAwesomeAnimation', [
+      state('small', style({
+        transform: 'scale(1)',
+      })),
+      state('large', style({
+          transform: 'scale(1.2)',
+      })),
+      transition('small => large', animate('1000ms ease-in',keyframes([
+        style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+        style({opacity: 1, transform: 'translateY(35px)',  offset: 0.5}),
+        style({opacity: 1, transform: 'translateY(0)',     offset: 1.0})
+      ]))),
+      transition('large => small', animate('100ms ease-in'))
+    ]),
+   ]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works 1!';
+  state: string = 'small';
+
+  animateMe() {
+    this.state = (this.state === 'small' ? 'large' : 'small');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule
   ],
